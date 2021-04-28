9256cad20863dedc06df6813dda694b67704e55f
yerin-ddadda <80931393+yerin-ddadda@users.noreply.github.com>
2021-04-29 07:48:42 +0900

    Clock part Commit

---
+++ clock.js
@@ -0,0 +1,16 @@
+const clockContainer = document.querySelector(".js-clock");
+const clockTitle = clockContainer.querySelector("h1");
+
+function loadTime(){
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    clockTitle.innerText = `${hours <10 ? `0${hours}` : hours}:${minutes<10 ? `0${minutes}` : minutes}:${seconds<10 ? `0${seconds}` : seconds}`;
+}
+
+function init(){
+    loadTime();
+    setInterval(loadTime, 1000);
+}
+init();
